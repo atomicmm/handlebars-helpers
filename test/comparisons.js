f4403687d532c900185af8afcbf5588f566dec95
Atomic <atomicmm@gmail.com>
2016-05-13 14:58:20 +0800

    add comparisons helper

---
+++ test/comparisons.js
@@ -0,0 +1,12 @@
+const assert = require('chai').assert;
+
+const helpers = require('../lib/exporter');
+
+describe('Main Test', () => {
+    describe('#comparisons', () => {
+        it('', () => {
+
+
+        });
+    });
+});
